0377b0939cc125d53413b930d4aca6ce7688357b
João Rocha <joaovictor75756@gmail.com>
2024-09-24 15:14:35 -0300

    Reajuste no método de capturarTecla

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -13,37 +13,41 @@ void Menu::capturarTecla()
 {
     while (this->flag.load())
     {
-        char tecla = _getch();
+        //char tecla = _getch();
+        pausar(200);
+        if (GetAsyncKeyState(VK_UP) & 0x8000) 
+        {
+            if (escolha == 0)
+            {
+                escolha = 1;
+                seta->moveTo(33, 180);
+            } else {
+                escolha = 0;
+                seta->moveTo(26, 180);
+            }
+        }
+        else if (GetAsyncKeyState(VK_DOWN) & 0x8000)
+        {
+            if (escolha == 0)
+            {
+                escolha = 1;
+                seta->moveTo(33, 180);
+            } else {
+                escolha = 0;
+                seta->moveTo(26, 180);
+            }
+        }
+        else if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
+            this->flag.store(false); // Sinaliza para a main que o programa deve encerrar
+            break;
+        }
 
-        pausar(45);
-        if ((GetAsyncKeyState(VK_UP) & 0x8000) && (escolha == 0) ) 
-        {
-            escolha = 1;
-        }
-        else if ((GetAsyncKeyState(VK_UP) & 0x8000) && (escolha == 1))
-        {
-            escolha = 0;
-        }
-        else if ((GetAsyncKeyState(VK_DOWN) & 0x8000) && (escolha == 0))
-        {
-            escolha = 1;
-        }
-        else if ((GetAsyncKeyState(VK_DOWN) & 0x8000) && (escolha == 1))
-        {
-            escolha = 0;
-        }
-        else if (tecla == 'a')
-            escolha = 10;
-        else if (tecla == 13)
+        if (GetAsyncKeyState(VK_RETURN) & 0x8000) 
         {
             this->flag.store(false);
             break;
         }
-        if (GetAsyncKeyState(VK_RETURN) & 0x8000) 
-        {
-            this->flag.store(false); // Sinaliza para a main que o programa deve encerrar
-            break;
-        }
+        
     }
 }
 
@@ -54,16 +58,16 @@ void Menu::pausar(int millissegundos)
 
 void Menu::init()
 {
-    this->menu = new ObjetoDeJogo("Menu", Sprite("rsc/menu.img"), 15, 155);
+    this->menu = new ObjetoDeJogo("Menu", Sprite("rsc/menu.img"), 20, 170);
     objs.push_back(menu);
 
-    this->seta = new ObjetoDeJogo("Seta", Sprite("rsc/setaEscolha.img"), 18, 160);
+    this->seta = new ObjetoDeJogo("Seta", Sprite("rsc/setaEscolha.img"), 26, 180);
     objs.push_back(seta);
 
-    this->start = new ObjetoDeJogo("Start", Sprite("rsc/start.img"), 22, 120);
+    this->start = new ObjetoDeJogo("Start", Sprite("rsc/start.img"), 27, 140);
     objs.push_back(start);
 
-    this->exit = new ObjetoDeJogo("Exit", Sprite("rsc/exit.img"), 29, 120);
+    this->exit = new ObjetoDeJogo("Exit", Sprite("rsc/exit.img"), 34, 140);
     objs.push_back(exit);
 
 }
@@ -85,7 +89,7 @@ unsigned Menu::run(SpriteBuffer &screen)
         this->draw(screen);
         this->show(screen);
 
-        pausar(150);
+        pausar(200);
         system("cls");
     }
     menu.join();
