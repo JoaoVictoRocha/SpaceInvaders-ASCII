668b6cbbfd06699ddc3b0649170714f10095c299
João Rocha <joaovictor75756@gmail.com>
2024-09-24 14:08:30 -0300

    Reajustando o modo de inicialização do jogo

--- src/Game.hpp
+++ src/Game.hpp
@@ -1,6 +1,7 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include "Menu.hpp"
 #include "FaseLevel1.hpp"
 #include "ASCII_Engine/Sprite.hpp"
 #include "ASCII_Engine/SpriteBuffer.hpp"
@@ -15,16 +16,25 @@ public:
     {
         SpriteBuffer buffer(350, 80);
 
+        Menu menu("Menu", Sprite("rsc/background.img"));
         FaseLevel1 fase1("Fase1", Sprite("rsc/background.img"));
 
-        fase1.init();
-        int ret1 = fase1.run(buffer);
-		if ( ret1 != Fase::GAME_OVER && ret1 != Fase::END_GAME){
-		
-			buffer.clear();
-		}
-		else
-			std::cout << "GAME OVER" << std::endl;
+        menu.init();
+        int val = menu.run(buffer);
+        buffer.clear();
+        if (val == Fase::LEVEL_1)
+        {
+            fase1.init();
+            int ret1 = fase1.run(buffer);
+            if ( ret1 != Fase::GAME_OVER && ret1 != Fase::END_GAME)
+            {
+                std::cout << "????";
+            }
+            else
+                std::cout << "GAME OVER" << std::endl;
+        } else {
+            std::cout << "Fim de jogo" << std::endl;
+        }
 
 
         buffer.clear();
