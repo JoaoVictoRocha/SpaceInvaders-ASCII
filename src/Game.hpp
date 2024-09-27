3bf227732a7628b2635c9f52e42c9f7e9c804b00
João Rocha <joaovictor75756@gmail.com>
2024-09-27 01:30:03 -0300

    aumentando tamanho do SpriteBuffer e ajustando exibição das fases

--- src/Game.hpp
+++ src/Game.hpp
@@ -3,6 +3,8 @@
 
 #include "Menu.hpp"
 #include "FaseLevel1.hpp"
+#include "FaseLevel2.hpp"
+#include "FaseLevel3.hpp"
 #include "ASCII_Engine/Sprite.hpp"
 #include "ASCII_Engine/SpriteBuffer.hpp"
 
@@ -14,31 +16,36 @@ public:
 
     static void run()
     {
-        SpriteBuffer buffer(350, 80);
+        SpriteBuffer buffer(420, 110);
 
         Menu menu("Menu", Sprite("rsc/background.img"));
         FaseLevel1 fase1("Fase1", Sprite("rsc/background.img"));
+        FaseLevel2 fase2("Fase2", Sprite("rsc/background.img"));
+        FaseLevel3 fase3("Fase3", Sprite("rsc/background.img"));
 
         menu.init();
-        int val = menu.run(buffer);
+        int level0 = menu.run(buffer);
+
         buffer.clear();
-        if (val == Fase::LEVEL_1)
+
+        if (level0 == Fase::LEVEL_1)
         {
             fase1.init();
-            int ret1 = fase1.run(buffer);
-            if ( ret1 != Fase::GAME_OVER && ret1 != Fase::END_GAME)
+            int level1 = fase1.run(buffer);
+            if ( level1 == Fase::LEVEL_COMPLETE)
             {
-                std::cout << "????";
+                buffer.clear();
+                fase2.init();
+                int level2 = fase2.run(buffer);
+                if (level2 == Fase::LEVEL_COMPLETE)
+                {
+                    buffer.clear();
+                    fase3.init();
+                    int level3 = fase3.run(buffer);
+                }
             }
-            else
-                std::cout << "GAME OVER" << std::endl;
-        } else {
-            std::cout << "Fim de jogo" << std::endl;
         }
-
-
         buffer.clear();
-
     }
 
 };
